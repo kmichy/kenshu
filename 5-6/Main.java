6f7582cbd0a7455f8211ca3c5e9805bd88f841d7
kmichy <39512398+kmichy@users.noreply.github.com>
2018-06-05 17:29:01 +0900

    Add files via upload

---
+++ 5-6/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.nextLine());
+        String b = sc.nextLine();
+        b.replaceFirst("^0+", "");
+        if(){
+            
+        }
+        // String[] c = b.split("[\\s]+");
+        // int[] y = new int[c.length];
+
+    }
+}
